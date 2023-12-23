f2f83f67068e675d3ea258e75b17621cbdb7645d
Nelson Mehlis <128640902+ThePyth0nKid@users.noreply.github.com>
2023-12-23 10:19:06 +0100

    Update ALLOWED_HOSTS in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%a0q8^vi#56&4v&h4p_y!egw2!i!^4y5+772c1s_a84oc$rgku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','blog-django-1-a9ab1ad03b7a.herokuapp.com']
 
 
 # Application definition
