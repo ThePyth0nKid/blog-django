268847c442eef053b9b1912c23ff264e05feaed8
Nelson Mehlis <128640902+ThePyth0nKid@users.noreply.github.com>
2023-12-23 10:13:33 +0100

    Update DEBUG and ALLOWED_HOSTS settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%a0q8^vi#56&4v&h4p_y!egw2!i!^4y5+772c1s_a84oc$rgku'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
